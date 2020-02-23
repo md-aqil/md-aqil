d1cf91d1d668aeee799bac022795e2c89743cafa
Md Aqil <aqil@wipl.co.in>
2020-02-23 20:28:31 +0530

    added

--- js/controller.js
+++ js/controller.js
@@ -186,3 +186,32 @@ ScrollOut({
 
 ScrollOut();
  
+var isOepn = false;
+var menuButton = document.querySelector('.menu-button');
+var swiper = new Swiper('.swiper-container.swiper-menu', {
+  slidesPerView: 'auto',
+  initialSlide: 1,
+  resistanceRatio: 0,
+  slideToClickedSlide: true,
+  on: {
+    init: function () {
+      var slider = this;
+      menuButton.addEventListener('click', function () {
+        if (isOepn) {
+          slider.slideNext();
+        } else {
+          slider.slidePrev();
+        }
+        isOepn = !isOepn;
+      }, true);
+    },
+    slideChange: function () {
+      var slider = this;
+      if (slider.activeIndex === 0) {
+        menuButton.classList.add('cross');
+      } else {
+        menuButton.classList.remove('cross');
+      }
+    },
+  }
+});
