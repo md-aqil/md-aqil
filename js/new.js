2e456a5f361631f41cb77d464fdd40f031c69f46
md-aqil <aqilali381@gmail.com>
2022-04-11 13:58:09 +0530

    added

--- js/new.js
+++ js/new.js
@@ -21,13 +21,13 @@ const tl = gsap.timeline({
 
   tl.from("#code-shadow", { scale:0, transformOrigin:'center', duration}, .1);
 
-tl.from("#coffee-square", { scale:0, transformOrigin:'center', duration: duration}, .3);
-tl.from("#coffee-shadow", { transformOrigin:'center', scale:0, duration: duration},.3);
-tl.from("#cofee-glass", {y: 350, scale:0, transformOrigin:'center', duration: duration}, .3);
+  tl.from("#coffee-square", { scale:0, transformOrigin:'center', duration: duration}, .3);
+  tl.from("#coffee-shadow", { transformOrigin:'center', scale:0, duration: duration},.3);
+  tl.from("#cofee-glass", {y: 350, scale:0, transformOrigin:'center', duration: duration}, .3);
 
 
-tl.from("#lapi-square", { transformOrigin:'center', y: 0, autoAlpha:0, scale:0, duration: duration}, .3);
-tl.from("#lapi-shadow", { transformOrigin:'center', y: 0, autoAlpha:0,  scale:0, duration: duration}, .1);
+   tl.from("#lapi-square", { transformOrigin:'center', y: -10, autoAlpha:0, scale:0, duration: duration}, .3);
+    tl.from("#lapi-shadow", { transformOrigin:'center', y: 0, autoAlpha:0,  scale:0, duration: duration}, .1);
 
 tl.from("#lapi-group", {y: -200, duration: duration, autoAlpha:0}, .3);
 
@@ -36,8 +36,3 @@ tl.from("#colored-line path", {duration: duration, transformOrigin:"left"}, .1);
 
 
 tl.from("#squre-2, #squre-3, #squre-4, #Group_12351, #Group_12352, #Ellipse_202, #Ellipse_196-2, #Group_12353", {scale:0, transformOrigin:'center', duration: 1},.5);
-
-document.querySelector("#restart").onclick = () => tl.restart();
-
-
-
